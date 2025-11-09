5684cf34d9ef4957b0e7e2e32fe96531b6ebdd10
Jens Laufer <jens.laufer@solytics.de>
2025-11-09 12:14:29 +0100

    test page

--- src/pages/Results.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <h2 class="text-brand1 text-2xl font-bold text-center">Results Page</h2>
-</template>
--- src/pages/TeachMultiplicative.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <h2 class="text-brand1 text-2xl font-bold text-center">Teach Multiplicative Dymamic</h2>
-</template>
