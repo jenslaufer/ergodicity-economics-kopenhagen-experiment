05acc297afc32cb3e8192bec0da56f3cd62d5097
Jens Laufer <jens.laufer@solytics.de>
2025-11-10 10:32:08 +0100

    changed home

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,21 +1,17 @@
 <template>
   <div class="flex flex-col items-center justify-center">
     <div class="max-w-3xl min-w-xl sm:px-6 lg:px-8">
-      <h1 class="text-brand1 text-4xl font-bold text-center mt-5">EE Economics: The Kopenhagen Experiment</h1>
-      <p class="text-brand text-xl font-normal text-center mt-8">The Kopenhagen experiment explores the fundamental
-        assumptions of ergodicity in economic systems, challenging the traditional economic models that assume ensemble
-        averages equal time averages.</p>
-      <p class="text-brand text-xl font-normal text-center mt-1">The goal of this project is to provide an online
-        version to the public.</p>
-      <p class="text-brand text-xl font-semibold text-center mt-3">Please be patient until it's ready...</p>
+      <h1 class="text-brand1 text-4xl font-bold text-center mt-5 mb-8">EE Economics: The Kopenhagen Experiment</h1>
       <teach-additive v-if="step === 0" />
       <play-additive v-else-if="step === 1" />
       <teach-multiplicative v-else-if="step === 2" />
       <play-multiplicative v-else-if="step === 3" />
       <results v-else-if="step === 4" />
-      <button @click="nextStep" v-if="step < 4"
-        class="mt-10 px-6 py-3 bg-brand1 text-brand0 font-semibold rounded hover:bg-gray-800 transition-colors duration-200">Next
-        >>></button>
+      <div class="flex justify-center">
+        <button @click="nextStep" v-if="step < 4"
+          class="mt-10 px-6 py-3 bg-brand1 text-brand0 font-semibold rounded hover:bg-gray-800 transition-colors duration-200">Next
+          >>></button>
+      </div>
     </div>
     <app-footer
       component-style="text- text-gray-600 hover:text-gray-800  transition-colors duration-200 font-medium underline"
@@ -32,7 +28,7 @@ import PlayMultiplicative from "./PlayMultiplicative.vue";
 import TeachMultiplicative from "./TeachMultiplicative.vue";
 import Results from "./Results.vue";
 
-const step = ref(8)
+const step = ref(0)
 
 const nextStep = () => {
   step.value += 1;
