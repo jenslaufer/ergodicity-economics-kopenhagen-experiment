2184450a0a8bbf7724e471754887ac2be64157c5
Jens Laufer <jens.laufer@solytics.de>
2025-11-11 10:14:59 +0100

    moved to only one teaching component

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,9 +2,9 @@
   <div class="flex flex-col items-center justify-center">
     <div class="max-w-3xl min-w-xl sm:px-6 lg:px-8">
       <h1 class="text-brand1 text-4xl font-bold text-center mt-5 mb-8">EE Economics: The Kopenhagen Experiment</h1>
-      <teach-additive v-if="step === 0" />
+      <teach title="Teach Additive Dymamic" v-if="step === 0" />
       <play-additive v-else-if="step === 1" />
-      <teach-multiplicative v-else-if="step === 2" />
+      <teach title="Teach Multiplicative Dymamic" v-else-if="step === 2" />
       <play-multiplicative v-else-if="step === 3" />
       <results v-else-if="step === 4" />
       <div class="flex justify-center">
@@ -22,10 +22,9 @@
 
 <script setup>
 import { ref } from "vue";
-import TeachAdditive from "./TeachAdditive.vue";
 import PlayAdditive from "./PlayAdditive.vue";
 import PlayMultiplicative from "./PlayMultiplicative.vue";
-import TeachMultiplicative from "./TeachMultiplicative.vue";
+import Teach from "./Teach.vue";
 import Results from "./Results.vue";
 
 const step = ref(0)
---
+++ src/pages/Teach.vue
@@ -0,0 +1,11 @@
+<template>
+    <h2 class="text-brand1 text-2xl font-bold text-center">{{ title }}</h2>
+</template>
+<script setup>
+const props = defineProps({
+    title: {
+        type: String,
+        required: true,
+    }
+});
+</script>
